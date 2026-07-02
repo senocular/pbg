1d3af3acc19b87d9caf046e525c84e8f65ea7b03
senocular <senocular@users.noreply.github.com>
2026-07-02 09:46:06 -0400

    support more controller buttons

--- src/apps/controller/index.js
+++ src/apps/controller/index.js
@@ -1,7 +1,10 @@
 import { keyboardMappings } from "../../components/pbg-controller/keyboardMappings.js";
 import "../../components/pbg-controller/PBGController.js";
 
+const options = document.getElementById("options");
 const kbdMappingSelect = document.getElementById("kbd-mapping-select");
+const layoutFaceCheck = document.getElementById("layout-face-check");
+const layoutShoulderCheck = document.getElementById("layout-shoulder-check");
 const controller = document.querySelector("pbg-controller");
 const text = document.querySelector("textarea");
 
@@ -18,6 +21,18 @@ function populateMappings() {
 kbdMappingSelect.onchange = () => {
     controller.applyKeyboardMapping(kbdMappingSelect.value);
     kbdMappingSelect.blur();
+};
+
+layoutFaceCheck.onchange = () => {
+    controller.classList.toggle("layout-4-face-buttons", layoutFaceCheck.checked);
+};
+
+layoutShoulderCheck.onchange = () => {
+    controller.classList.toggle("layout-4-shoulder-buttons", layoutShoulderCheck.checked);
+};
+
+options.onchange = (event) => {
+    event.target.blur();
 };
 
 const lines = [];
